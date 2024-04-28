7e2502b105161baa8d18976a35cb116896887565
Gudrun Omarsdottir <g.omarsdottir@gmail.com>
2024-04-28 17:44:50 +0000

    Fix views and urlpatterns

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+# Create your views here. "my_blog" from source code.
 def my_blog(request):
     return HttpResponse("Hello Blog!")
