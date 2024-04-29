33a605e72b0fa19a15f05d5481306eac203289ef
Gudrun Omarsdottir <g.omarsdottir@gmail.com>
2024-04-29 15:18:50 +0000

    Fix string

--- blog/models.py
+++ blog/models.py
@@ -29,9 +29,9 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now=True)
-    
+
     class Meta:
         ordering = ["created_on"]
 
     def __str__(self):
-        return f"Comment: {self.body} | written by {self.author}"
+        return f"Comment: {self.body} by {self.author}"
